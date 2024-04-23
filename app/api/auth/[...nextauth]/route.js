aebe2153b1dc134f85f79717cdd3cf77f042e74f
ash2228 <ashmeet2228@gmail.com>
2024-04-23 16:44:43 +0530

    dfs

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,8 +12,9 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ]
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 })
 
 export {handler as GET,handler as POST};
