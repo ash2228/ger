ff63f2c0df6174af5b71f65feeaa926fc930a08f
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-04-27 18:57:24 +0530

    try

--- app/actions/useractions.jsx
+++ app/actions/useractions.jsx
@@ -4,7 +4,7 @@ import payment from "../models/payment"
 import mongoose from "mongoose"
 
 export const initiate = async(amount,to_username,paymentForm,Name)=>{
-    await mongoose.connect(`${process.env.db}/payments`);
+    await mongoose.connect(`mongodb+srv://ashmeet:hdsotwas@payments.frhs4mg.mongodb.net/payments`);
     var instance = new Razorpay({ key_id: 'rzp_live_phNillhV9nNSIy', key_secret: 'm781uT18S4vNSfWpStfELQfA' })
     let options = {
         amount:Number.parseInt(amount),
--- app/support/page.jsx
+++ app/support/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import { React, useState } from "react";
 import "@/app/support/style.css";
 import Btn from "./btn";
